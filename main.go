b377dcd8191d3b9a45101ae7c4fdceb2b0052bcb
victor chiriac <victor.chiriac@gmail.com>
2025-02-05 14:02:27 +0200

    removed unused import

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/disintegration/imaging"
 )
 
